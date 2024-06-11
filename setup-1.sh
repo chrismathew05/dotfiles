35c3e2c033337040b6256a56e0fa3224c4e45e30
Chris Mathew <chrismathew05@gmail.com>
2024-06-11 16:57:11 -0400

    Added bat and moved things around for p10k

--- setup-1.sh
+++ setup-1.sh
@@ -5,7 +5,9 @@
 sudo apt update
 sudo apt install git-all -y
 sudo apt-get install python3-pip -y
+sudo apt install python3.10-venv -y
 sudo apt-get install ripgrep -y
+sudo apt install bat
 
 # vim/tmux customizations
 cp ./files/.vimrc ~/.vimrc
@@ -16,13 +18,13 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 tmux source ~/.tmux.conf
 
 # ohmyzsh + node + tldr
+sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash # latest from: https://github.com/nvm-sh/nvm?tab=readme-ov-file#installing-and-updating
-source ~/.zshrc
 nvm install node
+source ~/.zshrc
 sudo npm install -g tldr -y
 
 #powerlevel10k (manual)
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-source ~/.zshrc
-p10k configure
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+sudo p10k configure
